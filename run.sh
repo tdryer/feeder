8d704422a4c16742601c99c39c64082f6f4fdcbe
Tom Dryer <tomdryer.com@gmail.com>
2013-11-02 03:41:50 +0000

    Update message in run.py about installing tape

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # server the API server and public files using the reverse proxy "tape"
 
-tape --version >/dev/null 2>&1 || { echo >&2 "development server requires: https://github.com/wspringer/tape"; exit 1; }
+tape1 --version >/dev/null 2>&1 || { echo >&2 "Development server requires tape. Get it from https://github.com/wspringer/tape (version in pip is broken)."; exit 1; }
 
 PORT=8080
 API_SERVER_PORT=8081
