d082adfca5e709870cec5affc48ea3a3f23c5aaa
Justin Symonds <justinjfsymonds@hotmail.com>
2013-11-08 09:46:56 +0000

    #17 put all database files in separate module, implemented most of the basic functionality

--- feedreader/database/models.py
+++ feedreader/database/models.py
@@ -4,11 +4,12 @@ from sqlalchemy import Column, ForeignKey, Integer, Sequence, String
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
-    username = Column(String(50), primary_key=True)
-    password_hash = Column(String(255))
+    username = Column(String, primary_key=True)
+    password_hash = Column(String)
 
     def __init__(self, name, pwhash):
         self.username = name
@@ -17,16 +18,17 @@ class User(Base):
     def __repr__(self):
         return "<User('%s')>" % (self.username)
 
+
 class Feed(Base):
     __tablename__ = 'feeds'
 
-    id = Column(Integer, Sequence('feed_id_seq'), 
-primary_key=True)
-    title = Column(String(255))
-    feed_url = Column(String(255)) # feed resource
-    site_url = Column(String(255)) # main site
-    last_refresh_date = Column(String(30)) # HTTP format Sun, 06 Nov 1994 08:49:37 GMT
-    entries = relationship("Entry", order_by="Entry.date", backref="feed")
+    id = Column(Integer, Sequence('feed_id_seq'),
+                primary_key=True)
+    title = Column(String)
+    feed_url = Column(String)  # feed resource
+    site_url = Column(String)  # main site
+    last_refresh_date = Column(Integer)
+    #entries = relationship("Entry", order_by="Entry.date", backref="feed", cascade="all, delete, delete-orphan")
 
     def __init__(self, title, feed_url, site_url):
         self.title = title
@@ -37,16 +39,17 @@ primary_key=True)
     def __repr__(self):
         return "<RSSFeed('%s','%s')>" % (self.title, self.site_url)
 
+
 class Entry(Base):
     __tablename__ = 'entries'
 
     id = Column(Integer, Sequence('entry_id_seq'), primary_key=True)
     feed_id = Column(Integer, ForeignKey('feeds.id'))
-    content = Column(String(2048))
-    title = Column(String(255))
-    author = Column(String(50))
-    date = Column(String(10)) # format: YYYY-MM-DD
-    guid = Column(String(1024))
+    content = Column(String)
+    title = Column(String)
+    author = Column(String)
+    date = Column(Integer)
+    guid = Column(String)
 
     def __init__(self, feed_id, content, title, author, date):
         self.feed_id = feed_id
@@ -56,12 +59,18 @@ class Entry(Base):
         self.date = date
 
     def __repr__(self):
-        return "<FeedItem('%s','%s','%s')>" % (self.title, self.author, self.date)
+        return (
+            "<FeedItem('%s','%s','%s')>" % (self.title, self.author, self.date)
+        )
+
 
 class Subscription(Base):
     __tablename__ = 'subscriptions'
 
-    username = Column(String(50), ForeignKey('users.username'), primary_key=True)
+    username = Column(
+        String(50),
+        ForeignKey('users.username'),
+        primary_key=True)
     feed_id = Column(Integer, ForeignKey('feeds.id'), primary_key=True)
 
     def __init__(self, user, feed):
@@ -71,10 +80,14 @@ class Subscription(Base):
     def __repr__(self):
         return "<Subscription('%s','%s')>" % (self.username, self.feed_id)
 
+
 class Read(Base):
     __tablename__ = 'reads'
 
-    username = Column(String(50), ForeignKey('users.username'), primary_key=True)
+    username = Column(
+        String(50),
+        ForeignKey('users.username'),
+        primary_key=True)
     entry = Column(Integer, ForeignKey('entries.id'), primary_key=True)
 
     def __init__(self, user, entry_id):
