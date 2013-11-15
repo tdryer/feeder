60aa24d165f61c771c3c4d96b1437088b2deed31
David Yan <davidplusplus@gmail.com>
2013-11-15 22:02:03 +0000

    #28 Finished visuals for header bar and logout button

--- public/js/app/directives.js
+++ public/js/app/directives.js
@@ -3,6 +3,18 @@
   var directives = angular.module('feeder.directives', [])
 
   /**
+   * Displays the header bar.
+   *
+   * @directive
+   * @route '/home'
+   */
+  .directive('header', function() {
+    return {
+      templateUrl: 'partials/header.html'
+    }
+  })
+
+  /**
    * Displays the user's username.
    *
    * @directive
