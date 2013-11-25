41b70d673692ca7c05d09b449087927c97867ee0
Justin Lum <darktyger09@gmail.com>
2013-11-25 02:32:52 +0000

    Added descriptions for htmlToPlainText and anchorTarget filters

--- public/js/app/filters.js
+++ public/js/app/filters.js
@@ -1,6 +1,6 @@
 (function(angular, _) {
 
-  var filters = angular.module('feeder.filters', []);
+  angular.module('feeder.filters', [])
 
   /**
    * Exposes the power of Moment.js parsing and formatting.
@@ -10,7 +10,7 @@
    * @param {String} [format='LLL'] The format to format `date` with.
    * @returns {String} Returns `date` in the format `format`.
    */
-  filters.filter('moment', function() {
+  .filter('moment', function() {
     return function(date, format) {
       format || (format = 'LLL');
 
@@ -20,27 +20,42 @@
         date = moment(date);
       }
 
-      if (format.toUpperCase() == 'RELATIVE')
-      {
-          return date.fromNow();
+      if (format.toLowerCase() == 'relative') {
+        return date.fromNow();
       } else {
         return date.format(format);
       }
     }
-  });
+  })
 
-  filters.filter('htmlToPlainText', function() {
+  /**
+   * Converts HTML to plain text by stripping all HTML tags off.
+   *
+   * @filter
+   * @param {*} text The text to be stripped.
+   * @returns {String} Returns `text` without any HTML tags.
+   */
+  .filter('htmlToPlainText', function() {
     return function(text) {
       return String(text).replace(/<(?:.|\n)*?>/gm, '');
     }
-  });
+  })
 
-  filters.filter('setAnchorTarget', function() {
+  /**
+   * Parse through an HTML text locating all anchor tags and setting their
+   * target attribute.
+   *
+   * @filter
+   * @param {*} text The HTML text to parse.
+   * @param {String} value The value to set target to.
+   * @return {String} Returns the new HTML text with modified anchor tags.
+   */
+  .filter('anchorTarget', function() {
     return function(text, value) {
       // Remove new-lines
       var dom = angular.element(String(text).replace(/(\r\n|\r|\n)/gm, ''));
       value || (value = '_blank');
-      
+
       dom.find('a').attr('target', '_blank');
 
       return (angular.element('<div>').append(dom)).html();
