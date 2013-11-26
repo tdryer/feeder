fd61b23a9d2d0c8d83c6abe24bf5b940767a6507
Michael Tom-Wing <mtomwing@gmail.com>
2013-11-26 09:53:57 +0000

    @57 Added help messages to Config's ArgumentParser

--- feedreader/config.py
+++ feedreader/config.py
@@ -9,11 +9,16 @@ class Config(object):
         Create a Config instance from sys.argv.
         '''
         parser = ArgumentParser()
-        parser.add_argument('--amqp', default='')
-        parser.add_argument('--database', default='sqlite://')
-        parser.add_argument('--dummy', action='store_true', default=False)
-        parser.add_argument('--port', type=int, default=8080)
-        parser.add_argument('--updates', action='store_true', default=False)
+        parser.add_argument('--amqp', default='',
+                            help='AMQP connection string')
+        parser.add_argument('--database', default='sqlite://',
+                            help='SQLAlchemy database connection uri')
+        parser.add_argument('--dummy', action='store_true', default=False,
+                            help='enable dummy data')
+        parser.add_argument('--port', type=int, default=8080,
+                            help='run the server on this port')
+        parser.add_argument('--updates', action='store_true', default=False,
+                            help='enable periodic feed updates')
 
         args = parser.parse_args()
         return cls(args.amqp, args.database, args.dummy, args.port,
