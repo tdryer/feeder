a6b5ef74bf3243f92b01bcb731bbe7251d16bd6e
Michael Tom-Wing <mtomwing@gmail.com>
2013-11-26 09:38:04 +0000

    @57 Changed celery worker invocation to use new Config class
    Usage:
        python -m feedreader.tasks.worker [--amqp uri] [--updates] [--dummy]

--- feedreader/tasks/worker.py
+++ feedreader/tasks/worker.py
@@ -1,5 +1,14 @@
+from celery.bin.worker import worker
+
+from feedreader.config import Config
 from feedreader.tasks import Tasks
 
+if __name__ == '__main__':
+    config = Config.from_args()
+    tasks = Tasks(config.amqp_uri)
 
-tasks = Tasks(debug=False)
-app = tasks.app
+    kwargs = {
+        'events': True,
+        'loglevel': 'info',
+    }
+    worker(app=tasks.app).run(**kwargs)
