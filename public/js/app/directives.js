e62f32c6e9db9deaa1e467fd0b73f6db501e6e5d
David Yan <davidplusplus@gmail.com>
2013-11-30 18:07:43 +0000

    #85 and #125 fix added konami code goodeness and fixed loading animation css issue

--- public/js/app/directives.js
+++ public/js/app/directives.js
@@ -1,6 +1,40 @@
 (function() {
 
   /**
+   * Implements awesome konami code goodness.
+   *
+   * @directive
+   * @restrict attribute
+   */
+  this.directive('konami', function($http) {
+    var konami = '38,38,40,40,37,39,37,39,66,65'
+      , konami_class = 'konami-easter-egg'
+      , konami_len = 10;
+
+    return {
+      restrict: 'A',
+      link: function(scope, elem$) {
+        var document$ = angular.element(document)
+          , keys = [];
+
+        document$.on('keydown', function(event) {
+          keys.push(event.keyCode);
+
+          if (keys.toString().indexOf(konami) === 0) {
+            keys = [];
+            document$.off('keydown');
+            elem$.addClass(konami_class);
+          }
+
+          if (keys.length > konami_len) {
+            keys.shift();
+          }
+        });
+      }
+    }
+  });
+
+  /**
    * Overlays the entire screen if the application is loading.
    *
    * @directive
