6df6763cbd28e704d1a7f72380739f913f7f37be
Justin Symonds <justinjfsymonds@hotmail.com>
2013-11-30 02:24:56 +0000

    #91 changed Feeds POST handler to include a location header that can be used to fetch the feed metadata

--- feedreader/handlers.py
+++ feedreader/handlers.py
@@ -121,7 +121,10 @@ class FeedsHandler(APIRequestHandler):
             user = session.query(User).get(self.require_auth(session))
             yield self.subscribe_feed(session, user, self.celery_poller,
                                       self.tasks, body['url'])
-        # TODO: we should indicate the ID of the new feed (location header?)
+            feed = session.query(Feed).filter(
+                Feed.feed_url == body['url']
+            ).one()
+            self.set_header("Location", "GET /feeds/{}".format(feed.id))
         self.set_status(201)
 
 
