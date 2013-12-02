7a386e5af5819fbe6740d3ef3a808df633b02d2b
Tom Dryer <tomdryer.com@gmail.com>
2013-12-02 21:38:43 +0000

    Tweak CeleryPoller logs some more

--- feedreader/celery_poller.py
+++ feedreader/celery_poller.py
@@ -17,7 +17,6 @@ class CeleryPoller(object):
 
     @concurrent.return_future
     def run_task(self, task, *args, **kwargs):
-        logger.info("Running task '{}'".format(task))
         callback = kwargs["callback"]
         del kwargs["callback"]
 
@@ -26,14 +25,17 @@ class CeleryPoller(object):
         def result_callback(result): callback(result.get())
         result_callback = stack_context.wrap(result_callback)
 
+        logger.info("Starting task (should be fast)...")
         self._results_callbacks.append((task.delay(*args, **kwargs),
                                         result_callback))
+        logger.info("... task started")
         self._poll_tasks()
 
     def _poll_tasks(self):
         logger.debug("Polling Celery tasks")
         results_callbacks = []
         t = time.time()
+        logger.info("Starting task poll (should be fast)...")
         for result, callback in self._results_callbacks:
             if result.ready():
                 logger.info("Finished task")
@@ -44,7 +46,7 @@ class CeleryPoller(object):
                 logger.debug("Task is still pending")
                 results_callbacks.append((result, callback))
         t = time.time() - t
-        logger.info("Polled {} task(s) in {}ms"
+        logger.info("... polled {} task(s) in {}ms"
                     .format(len(self._results_callbacks), int(t * 1000)))
         self._results_callbacks = results_callbacks
         if len(self._results_callbacks) > 0:
